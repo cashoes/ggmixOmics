c58cb3bcbe40825a2ec9b5a3e2f10f9d69c86e1d
Casey Shannon <also.casey@gmail.com>
2017-06-16 18:20:03 -0700

    Flexible solution by wrapping extract_data in list.

--- R/ggcompplot.R
+++ R/ggcompplot.R
@@ -19,13 +19,14 @@ ggcompplot <- function(model, topn = NULL, combined = F) {
 }
 
 mixplot <- function(data, labs) {
-  ggplot(data) +
-    geom_point(aes(`comp 1`, `comp 2`, colour = class), size = 3) +
-    stat_ellipse(aes(`comp 1`, `comp 2`, colour = class),
-                 level = 0.68, linetype = 1, show.legend = F) +
-    # scale_color_few(name = '', labels = paste('DOL', c(0, 1, 3, 7))) +
-    labs(x = sprintf('component 1\n(%2.1f%% var. explained)', labs[1] * 100),
-         y = sprintf('component 2\n(%2.1f%% var. explained)', labs[2] * 100))
+  g <- ggplot2::ggplot(data, aes(`comp 1`, `comp 2`, colour = class)) +
+    ggplot2::geom_point(size = 3) +
+    ggthemes::scale_color_few(name = '', labels = paste('DOL', c(0, 1, 3, 7)), na.value = 'grey') +
+    ggplot2::labs(x = sprintf('component 1\n(%2.1f%% var. explained)', labs[1] * 100),
+                  y = sprintf('component 2\n(%2.1f%% var. explained)', labs[2] * 100))
+  if(!all(is.na(data$class)))
+    return(g + ggplot2::stat_ellipse(level = 0.68, linetype = 1, show.legend = F))
+  g
 }
 
 #' Extracts relevant data from the model object to plot.
@@ -36,6 +37,32 @@ mixplot <- function(data, labs) {
 #' @importFrom magrittr "%>%"
 extract_data <- function(model) UseMethod("extract_data")
 
+# unsupervised
+# non-sparse methods
+extract_data.pca <- function(model, comps = 1:2, class = NA) {
+  vars <- data.frame(model$variates$X[ , comps], class)
+  colnames(vars) <- c(paste('comp', comps), 'class')
+  labs <- model$explained_variance
+  list(list(vars = vars, labs = labs, vecs = NULL))
+}
+
+# sparse methods
+extract_data.spca <- function(model, comps = 1:2, class = NA) {
+  vars <- data.frame(model$variates$X[ , comps], class)
+  colnames(vars) <- c(paste('comp', comps), 'class')
+  labs <- model$explained_variance
+  vecs <- mixOmics::plotVar(model, plot = F)
+  list(list(vars = vars, labs = labs, vecs = vecs))
+}
+
+# supervised
+# non-sparse methods
+# extract_data.plsda <- function(model, comps = 1:2, class = NA) {}
+
+# sparse methods
+# extract_data.splsda <- function(model, comps = 1:2, class = NA) {}
+
+# multiblock methods
 extract_data.block.splsda <- function(model, comps = 1:2) {
   purrr:::pmap(.l = list(vars = head(model$variates, -1),
                  labs = head(model$explained_variance, -1),
