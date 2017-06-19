882b1b80065e0ecabf99dcf3eabdee0403514287
Casey Shannon <also.casey@gmail.com>
2017-06-19 15:27:40 -0700

    Hide extractor functions.

--- R/extractor.R
+++ R/extractor.R
@@ -1,38 +1,32 @@
 #' Extracts relevant data from the model object to plot.
 #' @author Casey Shannon
-#'
 #' @param model A mixOmics model object.
 #' @import tidyverse
 #' @import mixOmics
 #' @importFrom magrittr "%>%"
-extractor <- function(model, comps = 1:2) UseMethod("extractor")
+#' @export
+.extractor <- function(model, ...) UseMethod('.extractor')
 
-# unsupervised methods
-extractor.pca <- function(model, comps = 1:2, class = NA) {
-  # check that model is prcomp
-  stopifnot(any(class(model) %in% c('pca', 'spca')))
-  # extract values for specified components and return
-  df <- data.frame(model$variates$X[ , comps], class)
-  colnames(df) <- c(paste('comp', comps), 'class')
+#' @export
+.extractor.pca <- function(model, comps = 1:2) {
+  df <- data.frame(model$variates$X[ , comps])
+  colnames(df) <- c(paste0('comp', comps))
   df
 }
 
-# supervised methods
-extractor.DA <- function(model, comps = 1:2) {
-  # check that model is supervised
-  stopifnot(any(class(model) %in% c('plsda', 'splsda', 'block.splsda')))
-  # extract values for specified components and return
+#' @export
+.extractor.DA <- function(model, comps = 1:2) {
   df <- data.frame(model$variates$X[ , comps], class = model$Y)
-  colnames(df) <- c(paste('comp', comps), 'class')
+  colnames(df) <- c(paste0('comp', comps), 'class')
   df
 }
 
-# multiblock supervised methods
-extractor.sgccda <- function(model, comps = 1:2) {
+#' @export
+.extractor.sgccda <- function(model, comps = 1:2) {
   model$variates %>%
-    map(~ {
+    purrr::map(~ {
       df <- data.frame(.[ , comps], class = model$Y)
-      colnames(df) <- c(paste('comp', comps), 'class')
+      colnames(df) <- c(paste0('comp', comps), 'class')
       df
     })
 }
