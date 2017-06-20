9a85fbb51fea35627a9e228977e5b2154f6f74fa
Casey Shannon <also.casey@gmail.com>
2017-06-19 17:05:57 -0700

    More specific aes bindings in comp plot to make combined plot easier.

--- R/ggcompplot.R
+++ R/ggcompplot.R
@@ -36,13 +36,13 @@ ggcompplot.sgccda <- function(model, ...) {
 
 # helper - the plot function
 .compplot <- function(df, labs) {
-  g <- ggplot2::ggplot(df, ggplot2::aes_string(x = colnames(df)[1], y = colnames(df)[2], colour = 'class')) +
-    ggplot2::geom_point(size = 2) +
+  g <- ggplot2::ggplot(df) +
+    ggplot2::geom_point(ggplot2::aes_string(x = colnames(df)[1], y = colnames(df)[2], colour = 'class'), size = 2) +
     ggthemes::scale_color_few(name = '', na.value = 'grey') +
     ggplot2::labs(x = sprintf('%s\n(%2.1f%% var. explained)', colnames(df)[1], labs[1] * 100),
                   y = sprintf('%s\n(%2.1f%% var. explained)', colnames(df)[2], labs[2] * 100))
   if(!all(is.na(df$class)))
-    return(g + ggplot2::stat_ellipse(level = 0.68, linetype = 2, show.legend = F))
+    return(g + ggplot2::stat_ellipse(ggplot2::aes_string(x = colnames(df)[1], y = colnames(df)[2], colour = 'class'), level = 0.68, linetype = 2, show.legend = F))
   else
     g
 }
